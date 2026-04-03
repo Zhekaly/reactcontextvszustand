323cb0c9f6cfb153db353cbaaff28ad3dbf663f6
Zhekaly <154298441+Zhekaly@users.noreply.github.com>
2026-04-03 13:35:25 +0500

    Change App import to use main App component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-// Переключай комментарии чтобы тестировать разные реализации
-import App from './examples/ContextExample';
-//import App from './examples/ZustandExample';
+import App from './App';   // ← ВАЖНО
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-reportWebVitals();
